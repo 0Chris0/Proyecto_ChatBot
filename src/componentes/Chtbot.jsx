97c3a633eea00d47ce730cf4a504eb2d68fd02b1
Christian <cristianmaravilla59@gmail.com>
2026-02-17 13:23:43 -0600

    Proyecto chatbot React

---
+++ src/componentes/Chtbot.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Chtbot({ mensajes }) {
+// función para enviar mensaje del chatbot
+
+  return (
+    <div className="chat-container">
+      {mensajes.map((mensaje, indice) => (
+        // Clase cambia según si es BOT o USUARIO
+        <div key={indice} className={ mensaje.tipo === "usuario" ? "mensaje-wrapper usuario" : "mensaje-wrapper bot"}>
+          {/* Avatar SOLO para el bot */}
+          {mensaje.tipo === "bot" && (
+            <img src="./src/img/comentario.png" alt="Bot" className="bot-avatar"/>
+          )}
+          {/* Burbuja del mensaje */}
+          <div className={ mensaje.tipo === "usuario" ? "mensaje usuario": "mensaje bot"}>
+            <span>{mensaje.texto}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/datos/PR.js
@@ -0,0 +1,51 @@
+export const PR = [
+    //INIC. CONV.
+//-------------------------------------------------------------------------
+        /*INICIO*/
+  {
+    p: ["hola","ola","h0la","ho1a"],
+    r: "Hola!, en que te puedo ayudar?"
+  },
+  {
+    p: ["hey"],
+    r: "Hey!, dime en que te ayudo?"
+  },
+  {
+    p: ["man"],
+    r: "Que paso man, en que te ayudo?"
+  },
+  {
+    p: ["bro","br0"],
+    r: "Hola bro, en que te ayudo?"
+  },
+  {
+    p: ["ey"],
+    r: "Ey!, en que te puedo ayudar?"
+  },
+        /*FIN*/
+
+//-------------------------------------------------------------------------
+      //INIC. PREGUNTAS
+//-------------------------------------------------------------------------
+        /*INICIO*/
+  {
+    p: ["horario"],
+    r: "El horario de clase está en el sitio web oficial de la institucion"
+  },
+  {
+    p: ["calendario"],
+    r: "El calendario académico esta en el sitio web oficial de la institucion"
+  },
+  {
+    p: ["inscripcion","periodo","periodos"],
+    r: "La fecha del periodo de inscripcion para los cursos estaran en el sitio web oficial 3 a 4 semanas antes que inicie"
+  },
+  {
+  p: ["biblioteca"],
+  r: "La biblioteca está ubicada en el edificio A y los horarios de consulta son:\nlunes: 8:00 AM- 3:00 PM\nmartes: 9:00 AM- 4:00 PM\nmiércoles: 8:00 AM - 3:00 PM\njueves: 9:00 AM - 4:00 PM\nviernes: 8:00 AM- 3:00 PM"
+  },
+  {
+    p: [""],
+    r: " "
+  },
+];
